5e9242dd45d3ec2aae9cca58c12aa2c3bb0554ff
Phil Christensen <phil@bubblehouse.org>
2014-04-13 19:49:09 -0400

    setup openvpn vars

--- manifests/system/openvpn.pp
+++ manifests/system/openvpn.pp
@@ -1,11 +1,19 @@
 class autobuntu::system::openvpn {
   package { "openvpn":
     ensure => present
-  }
+  }->
   
   exec { "config-easy-rsa":
     command => "/bin/cp -r /usr/share/doc/openvpn/examples/easy-rsa/2.0 /etc/openvpn/easy-rsa",
     creates => "/etc/openvpn/easy-rsa",
-    require => Package['openvpn']
+  }->
+  
+  file { "/etc/openvpn/keys":
+    ensure => directory
+  }->
+  
+  file { "/etc/openvpn/easy-rsa/vars"
+    ensure => file,
+    content => template("autobuntu/system/openvpn/vars.sh.erb")
   }
 }
