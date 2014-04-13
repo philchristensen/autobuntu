f1e0d6c8f8f3c185ff7751737a228682c744d18b
Phil Christensen <phil@bubblehouse.org>
2014-04-13 19:38:16 -0400

    setup easy-rsa

--- manifests/system/openvpn.pp
+++ manifests/system/openvpn.pp
@@ -2,4 +2,10 @@ class autobuntu::system::openvpn {
   package { "openvpn":
     ensure => present
   }
+  
+  exec { "config-easy-rsa":
+    command => "/bin/cp -r /usr/share/doc/openvpn/examples/easy-rsa/2.0 /etc/openvpn/easy-rsa",
+    creates => "/etc/openvpn/easy-rsa",
+    require => Package['openvpn']
+  }
 }
