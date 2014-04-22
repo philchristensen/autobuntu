e6e9c4452352c7b8cccc891adda3af8d7f1dffd0
Phil Christensen <phil@bubblehouse.org>
2014-04-21 21:52:45 -0400

    moved named conf files to site-specific module

--- manifests/network/bind9.pp
+++ manifests/network/bind9.pp
@@ -1,6 +1,6 @@
 class autobuntu::network::bind9(
-  $options_template = 'autobuntu/network/bind9/named.conf.local.erb',
-  $local_template = 'autobuntu/network/bind9/named.conf.options.erb',
+  $options_template = nil,
+  $local_template = nil
 ){
   tag('provisioning')
 
