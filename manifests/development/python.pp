a7313b6d1ab4705e7e84af49d95a87c5928a1b01
Phil Christensen <phil@bubblehouse.org>
2014-04-30 20:46:29 -0400

    added virtualenv and buildout

--- manifests/development/python.pp
+++ manifests/development/python.pp
@@ -1,12 +1,59 @@
 class autobuntu::development::python {
-  ## need to ensure that we have an up to date distribute
-  ## at time of this mod, ubuntu happens to ship with:
-  ## 0.6.24dev-r0 which pip hates due to the non-numeric suffix
-  ## Since the issue is the distro shipped python lib, we only
-  ## force this out onto the global python path 
+  package { ["python", "python-dev", "python-pip", "python-virtualenv"]:
+    ensure => present
+  }
+
+  autobuntu::development::python::pip::package { "setuptools-git":
+    ensure => present,
+    package => "setuptools-git",
+    pip_path => "/usr/bin/pip",
+    require => Package["python-pip"]
+  }
+  
   autobuntu::development::python::pip::package { "distribute":
     ensure => latest,
     package => "distribute",
     pip_path => '/usr/bin/env pip'
+  }
+}
+
+define autobuntu::development::python::virtualenv($basename, $location) {
+  include autobuntu::development::python
+  
+  exec { "mkvirtualenv/${location}/${name}":
+    command => "virtualenv ${basename}",
+    creates => "${location}/${basename}",
+    cwd => $location,
+    logoutput => on_failure,
+    path => ['/usr/bin'],
+    require => Package["python-virtualenv"]
+  }->
+
+  wt::pip::package { "distribute/${location}/${name}":
+    ensure => present,
+    package => "distribute",
+    pip_path => "${location}/${basename}/bin/pip",
+    require => Package["python-pip"]
+  }
+}
+
+define autobuntu::development::python::buildout($location){
+  include autobuntu::development::python
+  
+  exec { "bootstrap/${location}":
+    command => "python bootstrap.py",
+    creates => "${location}/bin/buildout",
+    cwd => $location,
+    logoutput => on_failure,
+    path => ['/usr/bin']
+  }
+
+  exec { "buildout/${location}":
+    command => "python -S bin/buildout",
+    creates => "${location}/.installed.cfg",
+    cwd => $location,
+    logoutput => on_failure,
+    path => ['/usr/bin'],
+    require => Exec["bootstrap/${location}"]
   }
 }
