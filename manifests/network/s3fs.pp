2646383667ba2cb1c1aaebfb8d2733022db33cad
Phil Christensen <phil@bubblehouse.org>
2014-05-05 08:55:16 -0400

    pin to 1.77

--- manifests/network/s3fs.pp
+++ manifests/network/s3fs.pp
@@ -8,36 +8,36 @@ class autobuntu::network::s3fs {
     ensure => directory
   }->
   
-  staging::file { "s3fs-latest.zip":
-    source => "https://github.com/s3fs-fuse/s3fs-fuse/archive/master.zip"
+  staging::file { "s3fs-fuse-1.77.tar.gz":
+    source => "https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.77.tar.gz"
   }->
   
-  staging::extract { "s3fs-latest.zip":
+  staging::extract { "s3fs-fuse-1.77.tar.gz":
     target => "/opt/s3fs",
-    creates => "/opt/s3fs/s3fs-fuse-master",
+    creates => "/opt/s3fs/s3fs-fuse-1.77",
   }->
   
   exec { "s3fs-autogen":
-    cwd => "/opt/s3fs/s3fs-fuse-master",
-    command => "/opt/s3fs/s3fs-fuse-master/autogen.sh",
-    creates => "/opt/s3fs/s3fs-fuse-master/configure"
+    cwd => "/opt/s3fs/s3fs-fuse-1.77",
+    command => "/opt/s3fs/s3fs-fuse-1.77/autogen.sh",
+    creates => "/opt/s3fs/s3fs-fuse-1.77/configure"
   }->
   
   exec { "s3fs-configure":
-    cwd => "/opt/s3fs/s3fs-fuse-master",
-    command => "/opt/s3fs/s3fs-fuse-master/configure",
-    creates => "/opt/s3fs/s3fs-fuse-master/config.status"
+    cwd => "/opt/s3fs/s3fs-fuse-1.77",
+    command => "/opt/s3fs/s3fs-fuse-1.77/configure",
+    creates => "/opt/s3fs/s3fs-fuse-1.77/config.status"
   }->
   
   exec { "s3fs-make":
-    cwd => "/opt/s3fs/s3fs-fuse-master",
+    cwd => "/opt/s3fs/s3fs-fuse-1.77",
     command => "/usr/bin/make",
-    creates => "/opt/s3fs/s3fs-fuse-master/src/s3fs",
+    creates => "/opt/s3fs/s3fs-fuse-1.77/src/s3fs",
     timeout => 0
   }->
   
   exec { "s3fs-make-install":
-    cwd => "/opt/s3fs/s3fs-fuse-master",
+    cwd => "/opt/s3fs/s3fs-fuse-1.77",
     command => "/usr/bin/make install",
     creates => "/usr/local/bin/s3fs"
   }
