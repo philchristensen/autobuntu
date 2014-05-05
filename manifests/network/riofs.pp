7d843579d9d9998ebd4fbad369195d8a3cf49d21
Phil Christensen <phil@bubblehouse.org>
2014-05-05 09:51:24 -0400

    switch to riofs

--- manifests/network/riofs.pp
+++ manifests/network/riofs.pp
@@ -12,32 +12,32 @@ class autobuntu::network::riofs {
     source => "https://github.com/skoobe/riofs/archive/v0.6.tar.gz"
   }->
   
-  staging::extract { "riofs-0.6.tar.gz":
+  staging::extract { "riofs-v0.6.tar.gz":
     target => "/opt/riofs",
     creates => "/opt/riofs/riofs-0.6",
   }->
   
   exec { "riofs-autogen":
-    cwd => "/opt/riofs/riofs-0.6",
-    command => "/opt/riofs/riofs-0.6/autogen.sh",
-    creates => "/opt/riofs/riofs-0.6/configure"
+    cwd => "/opt/riofs/riofs-v0.6",
+    command => "/opt/riofs/riofs-v0.6/autogen.sh",
+    creates => "/opt/riofs/riofs-v0.6/configure"
   }->
   
   exec { "riofs-configure":
-    cwd => "/opt/riofs/riofs-0.6",
-    command => "/opt/riofs/riofs-0.6/configure",
-    creates => "/opt/riofs/riofs-0.6/config.status"
+    cwd => "/opt/riofs/riofs-v0.6",
+    command => "/opt/riofs/riofs-v0.6/configure",
+    creates => "/opt/riofs/riofs-v0.6/config.status"
   }->
   
   exec { "riofs-make":
-    cwd => "/opt/riofs/riofs-0.6",
+    cwd => "/opt/riofs/riofs-v0.6",
     command => "/usr/bin/make",
-    creates => "/opt/riofs/riofs-0.6/src/riofs",
+    creates => "/opt/riofs/riofs-v0.6/src/riofs",
     timeout => 0
   }->
   
   exec { "riofs-make-install":
-    cwd => "/opt/riofs/riofs-0.6",
+    cwd => "/opt/riofs/riofs-v0.6",
     command => "/usr/bin/make install",
     creates => "/usr/local/bin/riofs"
   }
