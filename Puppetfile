2ace6fca9d7f1e091fd500cc3b7a250893fc14a8
Phil Christensen <phil@bubblehouse.org>
2014-05-21 18:42:30 -0400

    setting up sphinxsearch support

--- Puppetfile
+++ Puppetfile
@@ -2,3 +2,4 @@ forge "https://forge.puppetlabs.com"
 
 mod "nanliu/puppet-staging"
   :github_tarball => 'nanliu/puppet-staging'
+mod "puppetlabs/apt"
---
+++ manifests/network/sphinxsearch.pp
@@ -0,0 +1,12 @@
+class autobunto::network::sphinxsearch {
+  apt::ppa { 'ppa:builds/sphinxsearch-rel21': }->
+  
+  package { "sphinxsearch":
+    ensure => present
+  }
+  
+  service { "sphinxsearch":
+    ensure => running,
+    enable => true
+  }
+}
