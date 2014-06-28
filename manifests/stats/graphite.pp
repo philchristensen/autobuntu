7f410a6c7f18b45183e15d1b562684bad31b4a0a
Phil Christensen <phil@bubblehouse.org>
2014-06-28 12:52:12 -0400

    deps fix

--- manifests/stats/graphite.pp
+++ manifests/stats/graphite.pp
@@ -2,31 +2,31 @@ class autobuntu::stats::graphite(){
   autobuntu::development::python::virtualenv {"graphite-virtualenv":
     basename => "virtualenv",
     location => "/opt/graphite"
-  }
+  }->
   
   autobuntu::development::python::pip::package { "graphite-setuptools-git":
     ensure => present,
     package => "setuptools-git",
     pip_path => "/opt/graphite/virtualenv/bin/pip",
-  }
+  }->
 
   autobuntu::development::python::pip::package { "graphite-ceres":
     ensure => present,
     package => "https://github.com/graphite-project/ceres/tarball/master",
     pip_path => "/opt/graphite/virtualenv/bin/pip",
-  }
+  }->
   
   autobuntu::development::python::pip::package { "graphite-whisper":
     ensure => present,
     package => "whisper",
     pip_path => "/opt/graphite/virtualenv/bin/pip",
-  }
+  }->
   
   autobuntu::development::python::pip::package { "graphite-carbon":
     ensure => present,
     package => "carbon",
     pip_path => "/opt/graphite/virtualenv/bin/pip",
-  }
+  }->
   
   autobuntu::development::python::pip::package { "graphite-web":
     ensure => present,
