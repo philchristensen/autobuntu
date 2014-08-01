7838d0311202bd0631e78e6877744548f8915024
Phil Christensen <phil@bubblehouse.org>
2014-07-31 21:45:51 -0400

    configure statsd to autostart

--- manifests/stats/statsd.pp
+++ manifests/stats/statsd.pp
@@ -12,13 +12,26 @@ class autobuntu::stats::statsd(
   autobuntu::development::git::checkout { "statsd":
     url => "https://github.com/etsy/statsd.git",
     path => "/opt/statsd",
-    dirname => "current"
+    dirname => "current",
+    revision => "1590bcf56ea1a3ac167f62fba3d599b65582d5ea"
   }->
   
   file { "/opt/statsd/current/config.js":
     ensure => file,
     source => $statsd_conf_source,
     owner => 'root',
-    group => 'staff'
+    group => 'staff',
+    notify => Service['statsd']
+  }
+  
+  file { "/etc/init/statsd":
+    ensure => file,
+    source => "puppet:///modules/autobuntu/stats/statsd/upstart.conf",
+    notify => Service['statsd']
+  }
+  
+  service { "statsd":
+    enable => true,
+    running => true
   }
 }
