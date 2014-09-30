7fcdc03ad25c452d3b629ba53f6143c4a8d4a028
Phil Christensen <pchristensen@freelancersunion.org>
2014-09-29 23:13:47 -0400

    finished xaz_asg_host

--- lib/puppet/parser/functions/xaz_asg_host.rb
+++ lib/puppet/parser/functions/xaz_asg_host.rb
@@ -2,20 +2,28 @@ require 'puppet'
 require 'aws-sdk'
 
 module Puppet::Parser::Functions
-  newfunction(:xaz_asg_host, :type => :rvalue) do |args|
-    id = lookupvar('ec2_instance_id')
-    az = lookupvar('ec2_placement_availability_zone')
+  newfunction(:xaz_asg_host, :type => :rvalue) do |asg_prefix|
+    current_az = lookupvar('ec2_placement_availability_zone')
     
     Aws.config[:credentials] = Aws::InstanceProfileCredentials.new
-    Aws.config[:region] = az.slice(0, -2)
+    Aws.config[:region] = current_az.slice(0, -2)
     
     client = Aws::AutoScaling::Client.new
     response = client.describe_auto_scaling_instances
-    valid_instances = response.auto_scaling_instances.reject { |instance|
-      if instance.instance_id == id:
+    
+    xaz_instances = response.auto_scaling_instances.reject { |i|
+      if i.health_status == "Unhealthy"
         return true
-      elsif instance.availability_zone == az
+      elsif i.availability_zone == current_az
+        return true
+      elsif i.auto_scaling_group_name.start_with?(asg_prefix)
+        return true
     }
-    return valid_instances.sample
+    
+    if(xaz_instances.empty?)
+      fail("There aren't any suitable instances in your AutoScaling Group.")
+    end
+
+    xaz_instances.sample
   end
 end
