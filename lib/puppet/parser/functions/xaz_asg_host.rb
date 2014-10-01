016f9dc0557061fbcb7004b9665c24d8e952c201
Phil Christensen <pchristensen@freelancersunion.org>
2014-09-30 20:33:35 -0400

    fixed xaz_asg_host, i hope

--- lib/puppet/parser/functions/xaz_asg_host.rb
+++ lib/puppet/parser/functions/xaz_asg_host.rb
@@ -5,6 +5,9 @@ module Puppet::Parser::Functions
   newfunction(:xaz_asg_host, :type => :rvalue) do |asg_prefix|
     current_az = lookupvar('ec2_placement_availability_zone')
     
+    current_az = 'us-east-1'
+    asg_prefix = 'dram-prod-app'
+    
     Aws.config[:credentials] = Aws::InstanceProfileCredentials.new
     Aws.config[:region] = current_az[0,current_az.length-1]
 
@@ -12,9 +15,9 @@ module Puppet::Parser::Functions
     response = client.describe_auto_scaling_instances
 
     xaz_instances = response.auto_scaling_instances.delete_if do |i|
-      i.health_status == "unhealthy"
-      || i.availability_zone == current_az
-      || ! i.auto_scaling_group_name.start_with?(asg_prefix)
+      i.health_status == "unhealthy" || 
+      i.availability_zone == current_az || 
+      ! i.auto_scaling_group_name.start_with?(asg_prefix)
     end
     
     return xaz_instances.sample
